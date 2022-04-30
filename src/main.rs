8d713dd4876537b4bfd7fc2e3b57aa3210b4f1f0
Evan Merlock <merlo010@umn.edu>
2022-04-30 12:40:09 -0500

    Switched ILP format and fixed error fmt

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@
 
 use std::{fs::File, io::{Read, Write}};
 
-use rocket::http::{Status, ContentType};
+use rocket::{http::{Status, ContentType}, State};
 use rusqlite::Connection;
+use serde::Deserialize;
 
-const SQLLITE_DB_LOCATION: &'static str = "./pihole-FTL.db";
-const TIME_CHECK_NAME: &'static str = "./last_time_check";
+const SQLLITE_DB_LOCATION: &'static str = "/pihole-FTL.db";
+const LOCK_FILE: &'static str = "/last_file_check";
 const LINE_DELIMITER: &'static str = "\n";
 const SQL_QUERY: &'static str = 
     "SELECT 
-        timestamp||substr(id,length(id)-2,3),
+        timestamp,
         type,
         status,
         domain,
@@ -20,7 +21,10 @@ const SQL_QUERY: &'static str =
     FROM 
         queries 
     WHERE 
-        id > ?1";
+        id > ?1
+    ORDER BY
+        id
+    LIMIT ?2";
 
 #[derive(Debug)]
 pub struct PiholeEntry {
@@ -35,44 +39,84 @@ pub struct PiholeEntry {
 
 }
 
+#[derive(Deserialize, Debug)]
+pub struct EnvConfig {
+    #[serde(default="get_default_limit")]
+    limit: u64,
+    #[serde(default="get_default_db_location")]
+    db_location: String,
+    #[serde(default="get_default_lock_file_location")]
+    lock_file: String,
+}
+
+fn get_default_limit() -> u64 {
+    5000
+}
+
+fn get_default_db_location() -> String {
+    SQLLITE_DB_LOCATION.to_string()
+}
+
+fn get_default_lock_file_location() -> String {
+    LOCK_FILE.to_string()
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![get_pihole_stats])
+    let cfg = match envy::prefixed("PIHOLEM_").from_env::<EnvConfig>() {
+        Ok(config) => config,
+        Err(error) => panic!("{:#?}", error)
+    };
+    rocket
+        ::build()
+        .mount("/", routes![get_pihole_stats])
+        .manage(cfg)
 }
 
 #[get("/")]
-fn get_pihole_stats() -> (Status, (ContentType, String)) {
-    let timecheck_file = File::open(TIME_CHECK_NAME);
+fn get_pihole_stats(config: &State<EnvConfig>) -> (Status, (ContentType, String)) {
+    let lock_file = File::open(config.lock_file.clone());
     let mut last_id: u64 = 0;
 
-    if timecheck_file.is_ok() {
+    if lock_file.is_ok() {
         let mut id_buf = String::new();
-        match timecheck_file.expect("wowie something really fucked up").read_to_string(&mut id_buf) {
+        match lock_file.expect("Could not open checking file").read_to_string(&mut id_buf) {
             Ok(_) => {},
-            Err(e) => return (Status::InternalServerError, (ContentType::Text, e.to_string())),
+            Err(e) => return (
+                Status::InternalServerError, 
+                (ContentType::Text, format_error("Failed to read lock file", e.to_string()))
+            ),
         };
-        last_id = match id_buf.parse() {
+        last_id = match id_buf.trim().parse() {
             Ok(item) => item,
-            Err(e) => return (Status::InternalServerError, (ContentType::Text, e.to_string())),
+            Err(e) => return (
+                Status::InternalServerError, (ContentType::Text, format_error("Failed to parse last ID", e.to_string()))
+            ),
         };
     }
 
-    let sqllite_connection = match Connection::open(SQLLITE_DB_LOCATION) {
+    let sqllite_connection = match Connection::open(config.db_location.clone()) {
         Ok(db_conn) => db_conn,
-        Err(e) => return (Status::InternalServerError, (ContentType::Text, e.to_string())),
+        Err(e) => return (
+            Status::InternalServerError, (ContentType::Text, format_error("Failed to open SQLLite connection", e.to_string()))
+        ),
     };
 
     let mut stmt = match sqllite_connection.prepare(SQL_QUERY) {
         Ok(results) => results,
-        Err(e) => return (Status::InternalServerError, (ContentType::Text, e.to_string())),
+        Err(e) => return (
+            Status::InternalServerError, (ContentType::Text, format_error("Failed to prepare SQL statement", e.to_string()))
+        ),
     };
 
-    let pihole_entries = match stmt.query_map([last_id], |row| {
+    let pihole_entries = match stmt.query_map([last_id, config.limit], |row| {
         let res = process_individual_value(row)?;
         Ok(res)
     }) {
         Ok(r) => r,
-        Err(e) => return (Status::InternalServerError, (ContentType::Text, e.to_string())),
+        Err(e) => return (
+            Status::InternalServerError, (ContentType::Text, format_error("Failed to execute query", e.to_string()))
+        ),
     };
 
     let mut buf = String::new();
@@ -81,7 +125,9 @@ fn get_pihole_stats() -> (Status, (ContentType, String)) {
     for item in pihole_entries {
         let pre_ilp = match item {
             Ok(i) => i,
-            Err(e) => return (Status::InternalServerError, (ContentType::Text, e.to_string())),
+            Err(e) => return (
+                Status::InternalServerError, (ContentType::Text, format_error("Failed to get entry from row", e.to_string()))
+            ),
         };
         let (ilp, id) = pihole_entry_to_ilp(pre_ilp);
         buf.push_str(&ilp);
@@ -92,44 +138,63 @@ fn get_pihole_stats() -> (Status, (ContentType, String)) {
         } 
     }
 
-    let mut new_timecheck_file = match File::create(TIME_CHECK_NAME) {
+    let mut new_lock_file = match File::create(config.lock_file.clone()) {
         Ok(file) => file,
-        Err(e) => return (Status::InternalServerError, (ContentType::Text, e.to_string())),
+        Err(e) => return (
+            Status::InternalServerError, (ContentType::Text, format_error("Failed to lock lockfile", e.to_string()))
+        ),
     };
     let id_str = format!("{}", next);
-    match new_timecheck_file.write_all(id_str.as_bytes()) {
+    match new_lock_file.write_all(id_str.as_bytes()) {
         Ok(_) => {},
-        Err(e) => return (Status::InternalServerError, (ContentType::Text, e.to_string()))
+        Err(e) => return (
+            Status::InternalServerError, (ContentType::Text, format_error("Failed to write to lockfile", e.to_string()))
+        )
     };
 
     (Status::Ok, (ContentType::Text, buf))
 }
 
+fn format_error(hdr: &'static str, msg: String) -> String {
+    format!("{}: {}", hdr, msg)
+}
+
 fn process_individual_value(row: &rusqlite::Row) -> Result<PiholeEntry, rusqlite::Error> {
 
+    let id = row.get(6)?;
 
-    let pre_time: String = row.get(0)?;
+    let pre_time: u64 = row.get(0)?;
+    let (hundreds, tens, ones) = extract_places(id);
+    let real_time: String = format!("{}{}{}{}000000", pre_time, hundreds, tens, ones);
 
     Ok(PiholeEntry {
-        time: pre_time.parse().expect("oops shit broke"),
+        time: real_time.parse().expect("oops shit broke"),
         query_type: row.get(1)?,
         status: row.get(2)?,
         domain: row.get(3)?,
         client: row.get(4)?,
         upstream: row.get(5)?,
-        id: row.get(6)?
+        id,
     })
 
 }
 
+fn extract_places(id: u64) -> (u64, u64, u64) {
+    let hundreds_place = (id / 100) % 10;
+    let tens_place = (id / 10) % 10;
+    let ones_place = id % 10;
+    (hundreds_place, tens_place, ones_place)
+}
+
 fn pihole_entry_to_ilp(pihole_entry: PiholeEntry) -> (String, u64) {
 
-    (format!("dnsquery,query_type=\"{}\",client=\"{}\",status=\"{}\" domain=\"{}\",upstream=\"{}\" {}", 
+    (format!("dnsquery,query_type=\"{}\",client=\"{}\",status=\"{}\",domain=\"{}\",upstream=\"{}\" id={} {}", 
         pihole_entry.query_type, 
         pihole_entry.client, 
         pihole_entry.status, 
         pihole_entry.domain, 
         pihole_entry.upstream, 
+        pihole_entry.id,
         pihole_entry.time
     ), pihole_entry.id)
 
